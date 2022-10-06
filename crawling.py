04d73b36ce9b2c99bcfd72a514e5eb9b6cdabbf8
Byun Dong Hyun <ngoodsamari@naver.com>
2022-10-06 13:55:52 +0900

    first_commit

---
+++ crawling.py
@@ -0,0 +1,164 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+from datetime import datetime
+
+class Crawling:
+    service_type_val = {"생활안정": "NB0301", "주거-자립": "NB0302", "보육-교육": "NB0303", "고용-창업": "NB0304",
+                    "보건-의료": "NB0305", "행정-안전": "NB0306", "임신-출산": "NB0307", "보호-돌봄": "NB0308",
+                    "문화-환경": "NB0309", "농립축산어업": "NB0310"}  # chk-type1
+    support_type_val = {"현금": "cash", "현물": "stuff", "이용권": "ticket", "서비스": "service", "기타": "etc"}  # rdo-type1
+    apply_type_val = {"온라인신청": "BF0101", "타사이트신청": "BF0102", "방문신청": "BF0103",
+                  "기타": "(!BF0101 !BF0102 !BF0103)"}  # rdo-type2
+
+    post_keys = ["chk-type1","rod-type1","rdo-type2"] #상세조회용 키(서비스분야, 지원형태, 신청방법)
+    super_url = "https://www.gov.kr"
+    main_url = "https://www.gov.kr/portal/rcvfvrSvc/svcFind/svcSearchAll"
+
+    now_d = datetime.today().strftime("%Y%m%d%H%M")
+
+    final_return_dict = {
+        "상세검색_서비스분야": [],
+        "상세검색_지원형태": [],
+        "상세검색_신청방법": [],
+
+        "게시글_id": [],
+        "게시글_url" : [],
+        "제목": [],
+        "요약": [],
+        "지역상위": [],
+        "지역하위": [],
+
+        "지원형태": [],
+        "신청기간": [],
+        "접수기관": [],
+        "전화문의": [],
+
+        "지원대상": [],
+        "선정기준": [],
+        "지원내용": [],
+
+        "신청기간": [],
+        "신청방법": [],
+        "제출서류": [],
+
+        "접수기관": [],
+        "문의처": [],
+        # "사이트주소":None,
+
+        "최종수정일": [],
+        "스크랩시간": []
+    }
+
+    def crawling(self):
+        for st in self.service_type_val:
+            for supt in self.support_type_val:
+                for at in self.apply_type_val:
+                    page_number=0
+                    while(1):
+                        url_keys = ["chk-type1", "rdo-type1", "rdo-type2","startCount"]
+                        url_vals = [self.service_type_val[st], self.support_type_val[supt], self.apply_type_val[at], str(page_number)]
+                        url = self.get_url(self.main_url, url_keys, url_vals)
+
+                        page = requests.get(url)  # get요청 후 응답 수신
+                        soup = bs(page.text, "html.parser")
+                        posts = soup.find_all('a', {"class": "btn btn-type3"})
+                        if not posts:
+                            break
+
+                        for post in posts:
+                            last_url = post["href"]
+                            url = self.super_url + last_url
+                            print(url)
+
+                            now_dict = {key:None for key in self.final_return_dict.keys()}
+                            now_dict = self.get_post_info(url,now_dict)
+                            now_dict["게시글_url"] = url
+                            now_dict["게시글_id"] = last_url.split("/")[-1].split("?")[0]
+                            now_dict["상세검색_서비스분야"] = st
+                            now_dict["상세검색_지원형태"] = supt
+                            now_dict["상세검색_신청방법"] = at
+                            now_dict["상세검색_신청방법"] = at
+                            now_dict["스크랩시간"] = self.now_d
+
+                            print(now_dict)
+                            for key,val in now_dict.items():
+                                self.final_return_dict[key].append(val)
+
+                        page_number+=12
+                        break
+
+        return pd.DataFrame(self.final_return_dict)
+
+    def get_url(self,main_url,url_keys,url_vals):
+        url = main_url+"?"
+        size = len(url_keys)
+
+        for i in range(size):
+            url+=url_keys[i]+"="+url_vals[i]
+            if i!=size-1:
+                url+="&"
+        return url
+
+    def replace_text(self,text,remove_text=["\r","\n","\t","\xa0"]):
+        '''
+        text에 필요없는 remove_text를 제거한다
+        '''
+        text = text.strip()
+        for r in remove_text:
+            text = text.replace(r,"")
+
+        text = text.lstrip("-").lstrip("=")
+        return text
+
+    def get_post_info(self,url,now_dict):
+        post_page = requests.get(url)
+        post_soup = bs(post_page.text, "html.parser")
+
+        title = post_soup.find('strong', {"class": "benefit-detail-title"}).text
+        now_dict["제목"] = title
+        detail = post_soup.find('p', {"class": "benefit-detail-desc"}).text
+        now_dict["요약"] = detail
+
+        a = post_soup.find("span",{"class":"tag-item type2"})
+        if a:
+            now_dict["지역상위"] = post_soup.find("span", {"class": "tag-item type2"}).text
+        else:
+            loc = post_soup.find("span", {"class": "tag-item type1"}).text
+            L = loc.split(" / ")
+            assert(len(L)<=2)
+            if len(L)>=2:
+                now_dict["지역상위"],now_dict["지역하위"] = L[0],L[1]
+            else:
+                now_dict["지역상위"] = L[0]
+
+        post_one = post_soup.find_all('li', {"class": "benefit-detail-box"})
+        for po in post_one:
+            k = po.span.text
+            value = self.replace_text(po.p.text)
+            now_dict[k] = value
+
+        post_two = post_soup.find_all('ul', {"class": "detail-content-inner"})
+        for po in post_two:
+            if po.span and po.p:
+                k = po.span.text
+                if k == "사이트주소":
+                    continue
+                else:
+                    value = self.replace_text(po.p.text)
+                now_dict[k] = value
+
+        post_three = post_soup.find_all('div', {"class": "detail-content-inner"})
+        for po in post_three:
+            if po.span.text=="문의처":
+                now_dict["문의처"] = self.replace_text(po.p.text)
+
+
+
+        last_update = post_soup.find_all("span", {"class": "last-info-cont"})[-1].text
+        now_dict["최종수정일"] = last_update
+
+        return now_dict
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+from crawling import Crawling
+from datetime import datetime
+import argparse
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--d", type=str, default=datetime.today().strftime("%Y%m%d"),
+                      help="크롤링을 시작할 날짜, 디폴트는 오늘, 폴더명이기도 함")
+    args.add_argument("--save_folder", type=str, default="C:/Users/KED/Desktop/py_project/cheonan_welfare_crawling/crawling_data",
+                      help="크롤링한 데이터를 저장하는 폴더 경로(스케줄러를 이용할 시 절대경로를 사용해야 한다)")
+    config = args.parse_args()
+
+    folder = config.save_folder
+    d = config.d
+
+    crawler = Crawling()
+    df = crawler.crawling()
+    df.to_csv(folder+"/"+d+".csv",index=False,encoding="utf-8-sig")
